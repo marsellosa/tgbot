e17e6319d58418e6cde9bf4e64dc88669eedae61
marsellosa <marcelo.llosa.m@gmail.com>
2020-07-12 01:00:49 -0400

    Corregido el main view

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 
+@staff_member_required
 def inicio_view(request):
     page_name = "apps/main/inicio.html"
-    # msg = request.user.first_name if request.user.is_authenticated() else ''
-    title = 'Inicio'
     context = {}
+    
     return render(request, page_name, context)
