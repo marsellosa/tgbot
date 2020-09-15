cd44038cf17baefd541a5abcc44706b091b11107
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2020-09-14 23:10:56 -0400

    corregidos errores en el admin

--- apps/registros/productos/admin.py
+++ apps/registros/productos/admin.py
@@ -1,20 +1,15 @@
 from django.contrib import admin
 from .models import Categoria, Sabor, Pais, Detalles
-from admin_auto_filters.filters import AutocompleteFilter
-
-
-class CategoriaFilter(AutocompleteFilter):
-    title = 'Categoria'  # display title
-    field_name = 'categoria'  # name of the foreign key field
 
 
 class CategoriaAdmin(admin.ModelAdmin):
     # this is required for django's autocomplete functionality
-    search_fields = ['nombre']
+    list_display = ['nombre', 'pais', 'activo']
+   
 
 
 class SaborAdmin(admin.ModelAdmin):
-    list_filter = [CategoriaFilter]
+    list_display = ['sabor', 'categoria', 'descripcion']
 
 
 admin.site.register(Categoria, CategoriaAdmin)
