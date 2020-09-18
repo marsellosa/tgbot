c5f8b034e40267564bc4525ed3d8fd7438377125
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2020-09-18 18:22:47 -0400

    tarjetas agregadas a la pagina de inicio

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from apps.registros.productos.models import Categoria, Detalles
+from apps.bot.models import User
 
 
 @staff_member_required
 def inicio_view(request):
     page_name = "apps/main/inicio.html"
-    context = {}
+    producto = Categoria.objects.get(nombre='BATIDO')
+    detalles = Detalles.objects.all()
+    users = User.objects.all()
+    context = {
+        'producto': producto,
+        'detalles' : detalles,
+        'users': users
+    }
 
     return render(request, page_name, context)
